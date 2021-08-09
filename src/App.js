2d4e02a9d3655e3a6b32ca0c90b4b5d31587c88c
Ismail Mohamad <ikmohamad19@gmail.com>
2021-08-09 21:02:34 +0100

    Created a List component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
+import Todo from "./Components/List";
 function App() {
-  return <div>Hello!</div>;
+  return (
+    <div>
+      <h1>Reading List</h1>
+      <Todo />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/Components/List.js
@@ -0,0 +1,13 @@
+function List() {
+  return (
+    <div className="card">
+      <h2>TITLE</h2>
+      <div className="actions">
+        <button className="btn">Delete</button>
+      </div>
+    </div>
+  );
+}
+
+//export makes the list function available so it can be used in other files
+export default List;
