66dfbd88b63920c377eaf7e75bb91abcd7dc12ff
Ismail Mohamad <ikmohamad19@gmail.com>
2021-08-09 23:10:42 +0100

    Created an event listener for the button

--- src/Components/List.js
+++ src/Components/List.js
@@ -1,10 +1,15 @@
 function List(props) {
+  function deleteHandler() {
+    return console.log("delete requested");
+  }
   return (
     <div className="card">
       <h2>{props.title}</h2>
       <h3>Author: {props.author}</h3>
       <div className="actions">
-        <button className="btn">Delete</button>
+        <button className="btn" onClick={deleteHandler}>
+          Delete
+        </button>
       </div>
     </div>
   );
